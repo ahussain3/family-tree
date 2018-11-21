76e9a8909ae1dee41651ad164d2f29204a009eb0
Awais Hussain <awais@wave.com>
2018-11-21 23:20:02 +0300

    Ability to add new, untethered nodes

--- backend/database.py
+++ backend/database.py
@@ -14,11 +14,13 @@ class Child(Relationship): pass
 class Partner(Relationship): pass
 
 # Create new entries
-def person(name, gender, *, residence, birth_year, death_year):
-    return Node(NodeType.PERSON, **locals())
+def add_person(name, gender, *, residence, birth_year, death_year):
+    person = Node(NodeType.PERSON.value, **locals())
+    graph.create(person)
+    return person
 
-def marriage(person_a, person_b, *, year_started, year_ended):
-    marriage = Node(NodeType.MARRIAGE, year_started=year_started, year_ended=year_ended)
+def add_marriage(person_a, person_b, *, year_started, year_ended):
+    marriage = Node(NodeType.MARRIAGE.value, year_started=year_started, year_ended=year_ended)
     Partner(marriage, person_a)
     Partner(marriage, person_b)
     return marriage
--- backend/schema.py
+++ backend/schema.py
@@ -3,11 +3,17 @@ import gql_types
 import database
 
 def resolver_for(cls, name):
-    def inner(f):
+    def decorator(f):
         # We just fill in the resolve_xxx function onto the class
         setattr(cls, f"resolve_{name}", f)
         return f
-    return inner
+    return decorator
+
+def mutator_for(cls):
+    def decorator(f):
+        cls.mutate = staticmethod(f)
+        return f
+    return decorator
 
 def neo_to_gql(cls, object):
     fields = {field: object.get(field) for field in cls._meta.fields.keys()}
@@ -34,10 +40,26 @@ class Marriage(gql.ObjectType):
     end_year = gql.Int()
     # children = gql.List(Person)
 
+class AddPerson(gql.Mutation):
+    class Arguments:
+        name = gql.String(required=True)
+        gender = gql_types.GenderType(required=True)
+        residence = gql.String()
+        birth_year = gql.Int()
+        death_year = gql.Int()
+
+    person = gql.Field(Person, required=True)
+
+    def mutate(self, info, **user_args):
+        return mutate_add_person(info, **user_args)
+
 class Query(gql.ObjectType):
     """Top level GraphQL queryable objects"""
     person = gql.Field(Person, name=gql.String())
     search_persons = gql.Field(gql.List(Person), name=gql.String())
+
+class Mutation(gql.ObjectType):
+    add_person = AddPerson.Field(required=True)
 
 # Resolvers
 @resolver_for(Query, "person")
@@ -52,5 +74,17 @@ def query_search_persons(self, info, *, name):
     persons = database.search_persons(name)
     return [neo_to_gql(Person, person) for person in persons]
 
-schema = gql.Schema(query=Query)
+# Mutators
+def mutate_add_person(info, *, name, gender, residence=None, birth_year=None, death_year=None):
+    result = database.add_person(
+        name=name,
+        gender=gender,
+        residence=residence,
+        birth_year=birth_year,
+        death_year=death_year
+    )
+    return AddPerson(person=neo_to_gql(Person, result))
+
+
+schema = gql.Schema(query=Query, mutation=Mutation)
 
