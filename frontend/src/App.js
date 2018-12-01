658494e49b8768d7dcb3953c6c5703f08d6497e4
Awais Hussain <awais@wave.com>
2018-12-01 11:32:18 +0000

    [frontend] Search bar shows full width at top of screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import {SearchBar} from './components/SearchBar.js';
+import { Container, Row, Col, FormGroup } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <SearchBar></SearchBar>
+      <Container>
+        <Row>
+          <Col>
+            <FormGroup>
+              <SearchBar></SearchBar>
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
